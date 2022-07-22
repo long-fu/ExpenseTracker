dd109e4259c6a8d672a488b26f1a3981916e810e
haoshuai <haoshuai9192@gmail.com>
2022-07-22 10:45:57 +0800

    Testt Theme

---
+++ ExpenseTracker/Extnsions.swift
@@ -0,0 +1,16 @@
+//
+//  Extnsions.swift
+//  ExpenseTracker
+//
+//  Created by haoshuai on 2022/7/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
